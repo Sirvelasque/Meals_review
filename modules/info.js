153a2d45fc66db7427b352b960c4ad17a1e83e85
Sirvelasque <velasquezwalter10@yahoo.es>
2022-03-25 13:44:59 -0600

    display like numbers

--- modules/info.js
+++ modules/info.js
@@ -1,10 +1,23 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=pork';
+const mealsUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=pork';
+const likesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/muC92fOKTY2CMO5MPWsS/likes/';
 
 export default class Info {
   getMeals = async () => {
     let meals = [];
-    const response = await fetch(url);
+    const response = await fetch(mealsUrl);
     meals = await response.json();
     return meals.meals;
+  }
+
+  getLikes = async () => {
+    let likes = [];
+    const likesHash = {};
+    const response = await fetch(likesUrl);
+    likes = await response.json().catch(() => false);
+    if(!likes) return false;
+    likes.forEach((e) => {
+      likesHash[e.item_id] = e.likes;      
+    });
+    return likesHash;
   }
 }
--- modules/ui.js
+++ modules/ui.js
@@ -1,19 +1,36 @@
+import { indexOf } from 'lodash';
 import Info from './info.js';
 
 const info = new Info();
 
-const display = (title, image) => {
+const display = (title, image, id) => {
+  // Create elements
   const section = document.getElementById('app');
   const box = document.createElement('div');
   const imageContainer = document.createElement('div');
   const textLike = document.createElement('div');
+  const likebox = document.createElement('div');
+  const likeBtn = document.createElement('div');
+  const likeCounter = document.createElement('div');
   const comment = document.createElement('button');
+
+  // Give them class
   box.classList.add('box');
   imageContainer.classList.add('image');
   textLike.classList.add('title_like');
+  likebox.classList.add('likebox')
+  likeCounter.classList.add(`likesCounter${id}`);
+  likeBtn.classList.add('like');
+
+  // Content
   imageContainer.innerHTML = `<img src='${image}'>`;
-  textLike.innerHTML = `<div class="title">${title}</div><div class='like'></div>`;
+  textLike.innerHTML = `<div class="title">${title}</div>`;
   comment.innerHTML = 'Comment';
+
+  // Append
+  likebox.appendChild(likeCounter);
+  likebox.appendChild(likeBtn);
+  textLike.appendChild(likebox);
   box.appendChild(imageContainer);
   box.appendChild(textLike);
   box.appendChild(comment);
@@ -22,15 +39,24 @@ const display = (title, image) => {
 
 const init = async () => {
   let meals = [];
+  let likes = {};
   meals = await info.getMeals();
+  likes = await info.getLikes();
   meals.forEach((item) => {
-    display(item.strMeal, item.strMealThumb);
+    display(item.strMeal, item.strMealThumb, item.idMeal);
+    like(item.idMeal, likes[item.idMeal])
   });
 };
 
-const likes = async () => {
+const like = async (id, num) => {
+  const counter = document.querySelector(`.likesCounter${id}`);
+  if(num){
+    counter.innerHTML = `${num}`;
+  }else{
+    counter.innerHTML = '0';
+  }
 };
 
 export {
-  init, likes,
+  init, like,
 };
