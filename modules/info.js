600cee83562a944aec3c774769b9fa031c3f14e9
Sirvelasque <velasquezwalter10@yahoo.es>
2022-03-24 19:10:27 -0600

    display food items

---
+++ modules/info.js
@@ -0,0 +1,13 @@
+
+const url ='https://www.themealdb.com/api/json/v1/1/filter.php?i=pork';
+
+export default class Info {
+
+  getMeals = async() => {
+    let meals = [];
+    const response = await fetch(url);
+    meals = await response.json();
+    
+    return meals.meals;
+  }
+}
